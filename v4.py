b4142c68a58503a1667e1eab43699d6899907638
Innokenty <memories53@mail.ru>
2022-11-17 00:32:14 +0900

    v4 complete

--- v4.py
+++ v4.py
@@ -1,34 +1,35 @@
-def main():
-    users = None
-    usersData = None
-    usersPasswords = None
+import json
 
-    Fils_Js = ["users.json", "usersData.json", "usersPasswords.json"]
-    for Jsons in Fils_Js:
-        with open(f'{Jsons}', 'w+') as file:
-            try:
-                users = json.loads(file.read())
-            except:
-                users = list()
-                print('empty json!')
- 
-    if not type(users) and not type(usersData) and not type(usersPasswords):
-        print(f'can\'t load data cause one of parameters is None : \n users : {type(users) is not None} \n usersData : { type(usersData) is not None} \n usersPasswords : {type(usersPasswords) is not None}')
-        exit(-1)
- 
+
+def readData(Json):
+    with open(Json, 'w+') as file:
+        try:
+            return json.loads(file.read())
+        except:
+            print('empty json!')
+            return list()
+
+
+def writeData(Json, type):
+    with open(Json, 'w') as file:
+        file.write(json.dumps(type))
+
+
+def main():
+    users = readData("users.json")
+    usersData = readData('usersData.json')
+    usersPasswords = readData('usersPasswords.json')
+
     users.append('John')
     usersData.append({
-        'name' : 'John',
-        'surname' : 'Wick'
+        'name': 'John',
+        'surname': 'Wick'
     })
     usersPasswords.append('love my dog')
- 
-    with open('users.json', 'w') as file:
-        file.write(json.dumps(users))
-    with open('usersData.json', 'w') as file:
-        file.write(json.dumps(usersData))
-    with open('usersPasswords.json', 'w') as file:
-        file.write(json.dumps(usersPasswords))
- 
-if __name__ == '__main__':
-    main()
+
+    writeData('users.json', users)
+    writeData('usersData.json', usersData)
+    writeData('usersPasswords.json', usersPasswords)
+
+
+main()
