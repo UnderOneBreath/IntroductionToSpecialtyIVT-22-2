5c540fc06da9bdcb821eb7de753cd04bac924d88
Innokenty <memories53@mail.ru>
2022-11-18 02:13:27 +0900

    done v5

--- v5.py
+++ v5.py
@@ -3,17 +3,8 @@ class request(object):
         self.__count_of_failures = 0
         self.__sended_successfully = False
 
-    def if_more_than_ten_failures(self):
-        return self.__count_of_failures >= 10
-
-    def can_retry(self):
-        if self.if_more_than_ten_failures():
-            return False
-        else:
-            return True
-
     def retry(self):
-        if self.__sended_successfully or not self.can_retry():
+        if self.__sended_successfully or not self.__count_of_failures < 10:
             return False
 
         self.send()
